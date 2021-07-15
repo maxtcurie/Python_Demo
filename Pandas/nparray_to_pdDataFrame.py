1379576c1330c49d19dcfee795bd1d89851f0925
maxcurie1996 <47543965+maxcurie1996@users.noreply.github.com>
2021-07-14 19:38:47 -0500

    3

--- Pandas/nparray_to_pdDataFrame.py
+++ Pandas/nparray_to_pdDataFrame.py
@@ -10,4 +10,12 @@ print(np_array)
 
 df = pd.DataFrame(np_array, columns=['Column1', 'Column2'])
 
+print(df)
 
+print('**************')
+
+x=np.arange(0,1,0.001)
+y=np.sin(x)
+df=pd.DataFrame(list(zip(x, y)), columns=['x', 'sin(x)'])
+
+print(df)
--- Test.py
+++ Test.py
@@ -1,14 +1,5 @@
+def f(x):
+	return x**2.
 
-nmax=4
-n_list=[]
-
-for n0 in range(nmax):
-	for n1 in range(nmax):
-		for n2 in range(nmax):
-			if n0+2*n1-3*n2==0:
-				n_list.append([n0,n1,n2])
-
-
-
-print(n_list)
-print(lne(n_list))
+a=f(2)
+print('a='+str(a))
