c869e2ed9b455e120a11a583fa73769c446f639b
maxcurie <47543965+maxtcurie@users.noreply.github.com>
2022-08-07 20:48:39 -0700

    2

--- Data_structure/list_comprehension.py
+++ Data_structure/list_comprehension.py
@@ -9,3 +9,13 @@ print(a)
 a=[i*j  for i in range(10)\
 		for j in range(10)]
 print(a)
+
+
+a=['even' if i%2==0 else 'odd'  for i in range(10)]
+print(a)
+
+a=[0 if i%3==0 \
+	else 1 if i%3==1 \
+	else 2  \
+	for i in range(10)]
+print(a)
