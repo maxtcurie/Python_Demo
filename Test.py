28d855112f2a7bd8a5a84682a34ecd8f4b9c52cd
maxcurie <47543965+maxtcurie@users.noreply.github.com>
2022-08-31 10:24:11 -0700

    3

--- Test.py
+++ Test.py
@@ -1,12 +1,5 @@
-print('Hello, world!')
 import numpy as np
 import pandas as pd
 
-x_list=np.array([0.955]*5)
-m_list=np.array([5]*5)
-x_peak=0.195
-a=np.array([x_list, m_list,abs(x_list-x_peak)])
-b=a.transpose()
-df=pd.DataFrame(b,columns = ['x_list','m_list','peak_distance'])
-
-print(df)
+for i in range(1,9):
+	print(i)
