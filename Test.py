527ac75e155b7307c05d2d37aea861ea5c87a5b4
maxcurie <47543965+maxtcurie@users.noreply.github.com>
2022-09-13 09:02:50 -0700

    Added png to pdf
    This script look throught png files and convert to pdf file. This can be used for prepare for journal article upload.

--- Test.py
+++ Test.py
@@ -1,5 +1,13 @@
 import numpy as np
 import pandas as pd
 
-for i in range(1,9):
-	print(i)
+q=5.
+mi_me=1836.152*2.
+e=1.5
+factor=1./q*((mi_me)**0.5)*(e**2.5)/8.
+
+# 8 types of collsions ee, ei, ie, ii, iz ....
+
+print(factor)
+print(0.15*factor)
+print(1.2*factor)
